3d7ab6717b1f027d8043e55a3ec837d1d8aedcd4
wk2poor <81670847+wk2poor@users.noreply.github.com>
2022-04-29 14:38:13 +1000

    Add files via upload

---
+++ CAT.py
@@ -0,0 +1,78 @@
+#SLIGHT DETAILS
+print("(+================================ STORY GENERATOR 2.0 ====================================+)")
+print("(                                                                                                                                                                                     )")
+print("(                                                                                                                                                                                     )")
+print("(                                RULES :                                                                                                                                        )")
+print("(                                 please type with no capitals                                                                                                          )")
+print("(                                  or plurals                                                                                                                                     )")
+print("(                                   Have Fun!                                                                                                                                  )")
+print("(                                                                                                                                                                                    )")
+print("(                                                                                                                                                                                    )")
+print("(                                                                                                                                                                                    )")
+#inputs
+name = input("please type your name \n")
+if name == "max":
+    print("daddy")
+if name == "novak":
+    print("get those airpods out of your ears novak.")
+elif name == "bailey":
+    print("Hypixel Bedwars Master Has entered the chat")
+elif name == "jonah":
+    print("When are you gonna apply for mod in mha discord server")
+elif name == "ben":
+    print("I LOVE LEAN 💜💜💜")
+elif name == "bob":
+    print("BOB THE BUILDER CAN HE FIX IT")
+elif name == "obama":
+    print("i love obama prob the best president tbh")
+    print("Obama Care X obama Cares ✔")
+else:
+    print("Thats a cool name i like it to be honest "+ name+" sounds like a very cool name")
+    
+color = input("please type your LEAST Fav Color \n")
+if color == "red":
+    print("wow i really hate red too")
+elif color == "blue":
+    print("hey, i really like the color blue not cool >:(")
+elif color == "yellow":
+    print("agreed yellow stinks its too bright an i dont like bananas soooooo 🍌🍌🍌🍌")
+elif color == "black":
+    print("why you got a problem with black huh? ( ͠° ͟ʖ ͡° )")
+elif color == "brown":
+    print("why you got a problem with brown huh? ( ͠° ͟ʖ ͡° )")
+else:
+    print("True very True "+ color+" stinks, and you do too "+ name+".")
+
+animal = input("please type your pets name ;) \n")
+if animal == "pj":
+    print("my cats name is pj its such a cool name ^^ 🐈 🐈 🐈")
+    
+elif animal == "miguel":
+    print("I LOVE MIGUEL <3333 🐈 🐈 🐈 🐈")
+else:
+    print("Thats a cool name for a pet, "+animal +" you say there name was, huh? how cool! now")
+objecty = input("please type any object \n")
+if objecty == "coat":
+    print("how boring...")
+elif objecty == "laptop":
+    print("oOoOOOOOooO what brand is it o-o")
+else:
+    print("Nice! i love "+objecty +"s")
+#very funny story :)
+print("Alright So, one day there was a young boi named", animal)
+print("he loooooovvvveeeed the color", color)
+print("but you HATE, the color", color)
+print("SOOOOooooOOOOoo", animal)
+print("went to the store and bought a", objecty)
+print("BUT It was in the color", color)
+print("AND YOU HATE", color)
+print("so you go back to the store and get a refund for the", objecty)
+print("then you go back home and everything is better with you and", animal)
+print("==========================================================================================)")
+print("████████╗██╗  ██╗███████╗███████╗███╗   ██╗██████╗ ")
+print("╚══██╔══╝██║  ██║██╔════╝██╔════╝████╗██║██╔══██╗")
+print("       ██║   ███████║█████╗      █████╗  ██╔██╗ ██║██║  ██║ ")
+print("       ██║   ██╔══██║██╔══╝      ██╔══╝  ██║╚██╗██║██║  ██║ ")
+print("       ██║   ██║  ██║███████╗    ███████╗██║ ╚████║██████ ")
+print("       ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═══╝╚═════╝ ")
+                                                      
